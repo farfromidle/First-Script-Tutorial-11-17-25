873e6f89208f8f42074a10b9364fe30c8b0a9e3f
Derrick <derrick@idle.digital>
2025-11-28 04:41:04 -0600

    Init
    Initial commit for script tutorial

---
+++ sprite_2d.gd
@@ -0,0 +1,37 @@
+extends Sprite2D
+
+var speed = 400
+var angular_speed = PI
+
+func _ready():
+	var timer = get_node("Timer")
+	timer.timeout.connect(_on_timer_timeout)
+	
+#func _process(delta):
+	#var direction = 0
+	#if Input.is_action_pressed("ui_left"):
+		#direction = -1
+	#if Input.is_action_pressed("ui_right"):
+		#direction = 1
+
+	#rotation += angular_speed * direction * delta
+
+	#var velocity = Vector2.ZERO
+	#if Input.is_action_pressed("ui_up"):
+		#velocity = Vector2.UP.rotated(rotation) * speed
+
+	#position += velocity * delta
+	#var velocity = Vector2.UP.rotated(rotation) * speed
+
+	#position += velocity * delta
+	#Remeber indention matters...
+func _process(delta):
+	rotation += angular_speed * delta
+	var velocity = Vector2.UP.rotated(rotation) * speed
+	position += velocity * delta
+
+func _on_button_pressed() -> void:
+	set_process(not is_processing()) # Replace with function body.
+
+func _on_timer_timeout():
+	visible = not visible
